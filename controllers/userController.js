22a4332313bcf5a1afd25517e6f2ca35f7d167b2
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-02-01 23:37:16 +0530

    after adding changepassword route and checkAuth MW

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,6 @@ import Jwt  from "jsonwebtoken";
 class UserController{
     static userRegistration = async(req, res)=>{
         const {name, email, password, password_confirmation, tc} = req.body;
-        console.log(name, email, password, password_confirmation, tc);
         const user = await UserModel.findOne({email : email});
         if(user){
             res.send({status : "failed", message : "Email already exists"});
@@ -20,8 +19,10 @@ class UserController{
                         password : hashedPassowrd,
                         tc : Boolean(tc)
                     })
-                    await doc.save()
-                    res.status(200).send({status : "success", msg : "User Created Successfully" })
+                    await doc.save();
+                    const savedUser = await UserModel.findOne({email : email});
+                    const token =  Jwt.sign({userId : savedUser._id, userEmail : savedUser.email}, process.env.JWT_SECRET_KEY, {expiresIn : '5d'});
+                    res.status(201).send({status : "success", message : "User Created Successfully", token : token })
                     }catch(err){
                         res.send({status : "failed", message : "User not created", err : err})
                     }
@@ -33,6 +34,51 @@ class UserController{
             }
         }
     } 
+
+    static userLogin = async (req, res)=>{
+        const {password, email} = req.body
+        if(password && email){
+            try{
+                const user = await UserModel.findOne({email : email});
+                if(user){
+                  const isMatch = await bcrypt.compare(password, user.password);
+                  if(isMatch && user.email === email){
+                    const token  = Jwt.sign({userId : user._id, email : user.email}, process.env.JWT_SECRET_KEY);
+                    res.send({status : "success", token : token, message : "Loged in successfully"});
+                  }else{
+                    res.send({status : "failed", message : "Email or Passowrd is wrong"})
+                  }
+                }else{
+                    res.send({status : "failed", message : "Email does not exists"})
+                }
+            }catch(err){
+               res.send({starus : "failed", message : "something went wrong", error : err})
+            }
+        }else{
+            res.send({status : "failed", message : "All fields are required"})
+        }
+        
+    }
+
+    static resetUserPassword = async (req, res)=>{
+        const {password,  password_confirmation} = req.body;
+        if(password && password_confirmation){
+           if(password === password_confirmation){
+              try{
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassowrd = await bcrypt.hash(password, salt);
+                await UserModel.findByIdAndUpdate(req.user._id, {password : hashedPassowrd});
+                res.send({status : "success", message : "Password changed successfully" })
+              }catch(err){
+                  res.send({status : "failed", message : "something went wrong try again"})
+              }
+           }else{
+               res.send({status : "failed", message : "both password does not match"})
+           }
+        }else{
+            res.send({status : "failed", message : "All fields are required"})
+        }
+    }
 }
 
 export default UserController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import UserController from '../controllers/userController.js';
+import checkUserAuth from '../middlewares/checkUserAuth.js';
 const router = express.Router();
 
+//public routes
 router.post('/register' , UserController.userRegistration);
+router.post('/login', UserController.userLogin);
+
+//protected routes
+router.post('/changepassword', checkUserAuth ,UserController.resetUserPassword)
 
 export default router;
